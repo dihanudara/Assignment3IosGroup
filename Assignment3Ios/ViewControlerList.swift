3db8c2333c0a0606ddb909189bc7c13ccf20a11e
Michael <mjmanning25@gmail.com>
2019-11-08 20:48:58 +1300

    tried to add a iboutlet, it didnt work

---
+++ Assignment3Ios/ViewControlerList.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControlerList.swift
+//  Assignment3Ios
+//
+//  Created by Onisha Juhl on 8/11/19.
+//  Copyright © 2019 HEWA DEWAGE DIHAN UDARA SANDARUWAN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class ViewControllerList: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}

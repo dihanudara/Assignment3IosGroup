fb28c4537de43caa8e8d5905aa9e8d56a69c1200
dihan <dihanudara@gmail.com>
2019-11-11 22:27:30 +1300

    Final project

---
+++ Assignment3IosTests/Assignment3IosTests.swift
@@ -0,0 +1,20 @@
+//
+//  Assignment3IosTests.swift
+//  Assignment3IosTests
+//
+//  Created by HEWA DEWAGE DIHAN UDARA SANDARUWAN on 11/11/2562 BE.
+//  Copyright © 2562 HEWA DEWAGE DIHAN UDARA SANDARUWAN. All rights reserved.
+//
+
+import XCTest
+
+class Assignment3IosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+    }
+
+
+
+}
---
+++ Assignment3IosTestss/Assignment3IosTestss.swift
@@ -0,0 +1,33 @@
+//
+//  Assignment3IosTestss.swift
+//  Assignment3IosTestss
+//
+//  Created by HEWA DEWAGE DIHAN UDARA SANDARUWAN on 11/11/2562 BE.
+//  Copyright © 2562 HEWA DEWAGE DIHAN UDARA SANDARUWAN. All rights reserved.
+//
+
+import XCTest
+
+class Assignment3IosTestss: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
